a54143be4fc1235d5a8e399eda8e4a36e1cf781a
ajay <ajaysunarthi@iitj.ac.in>
2016-11-12 22:13:01 +0530

    Mocha :heart:

---
+++ MochaRGB/index.js
@@ -0,0 +1,19 @@
+describe('Color Code Converter', function() {
+
+    describe('RGB to Hex conversion', function() {
+        
+        it('converts the basic colors', function() {
+
+        });
+
+    });
+
+    describe('Hex to RGB conversion', function() {
+
+    	it('converts the basic colors', function() {
+
+        });
+
+    });
+
+});
